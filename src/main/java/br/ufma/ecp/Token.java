56767a0697ef6a01a8acd040db9c309163dd88f4
Emanuelle Lemos <manuluz.lemos@gmail.com>
2022-05-09 07:15:53 -0300

    feat(): adicionando categorias de token

--- src/main/java/br/ufma/ecp/Token.java
+++ src/main/java/br/ufma/ecp/Token.java
@@ -1,5 +1,6 @@
 package br.ufma.ecp;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Token {
@@ -7,12 +8,31 @@ public class Token {
     final TokenType type;
     final String lexeme;
 
+    List<String> symbols = Arrays.asList(
+        "+", "-", ".", "*", "/", "&", "|", "<", ">", 
+        "=", "~", "{", "}", "(", ")", "[", "]", ",", ";");
+    List<String> keywords = Arrays.asList(
+        "while", "class", "constructor", "function", "method",
+        "field", "static", "var", "int", "char", "boolean", "void", 
+        "true", "false", "null", "this", "let", "do", "if", "else", "return");
+    
     public Token(TokenType type, String lexeme){
         this.type = type;
         this.lexeme = lexeme;
     }
 
     public String toString(){
-        return "<" + type + ">" + lexeme + "</" + type + ">"; 
+        String categoria = type.toString();
+        if(symbols.contains(lexeme)){
+            categoria = "symbol";
+        } else if(keywords.contains(lexeme)){
+            categoria = "keyword";
+        } else if (categoria == "NUMBER"){
+            categoria = "intConst";
+        } else if (categoria == "STRING"){
+            categoria = "stringConstant";
+        }
+
+        return "<" + categoria + ">" + lexeme + "</" + categoria + ">"; 
     }
 }
