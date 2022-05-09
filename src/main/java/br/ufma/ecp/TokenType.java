13a51a8ecb178ebdb01d109a353b4b9de47538aa
Emanuelle Lemos <manuluz.lemos@gmail.com>
2022-05-09 06:40:01 -0300

    feat(): add tokens

--- src/main/java/br/ufma/ecp/TokenType.java
+++ src/main/java/br/ufma/ecp/TokenType.java
@@ -1,14 +1,57 @@
 package br.ufma.ecp;
 
 public enum TokenType {
-    PLUS, MINUS,
+    // symbols - operators
+    PLUS, // '+'
+    MINUS, // '-'
+    DOT, // '.'
+    ASTERISK, // '*'
+    SLASH, // '/' 
+    AND, // '&' equivalente a &amp;
+    OR, // '|' 
+    LT, // '<' equivalente a &lt;
+    BT, // '>' equivalente a &gt/
+    ASSIGN, // '='
+    NOT, // '~'
+    
+    // symbols - delimiters
+    LBRACE, // '{'
+    RBRACE, // '}'
+    LPAREN, // '('
+    RPAREN, // ')'
+    LBRACKET, // '['
+    RBRACKET, // ']'
+    COMMA, // ','
+    SEMICOLON, // ';'
 
     NUMBER,
 
     IDENTIFIER,
 
+    STRING,
+
     // keywords
     WHILE,
+    CLASS, 
+    CONSTRUCTOR, 
+    FUNCTION, 
+    METHOD, 
+    FIELD, 
+    STATIC, 
+    VAR, 
+    INT, 
+    CHAR, 
+    BOOLEAN, 
+    VOID, 
+    TRUE, 
+    FALSE, 
+    NULL, 
+    THIS, 
+    LET, 
+    DO, 
+    IF, 
+    ELSE, 
+    RETURN,
 
     EOF,
 
